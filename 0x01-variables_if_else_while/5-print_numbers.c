812bf1ec34949124d0e3d84c567ec22e29c6e05a
Sideeqhammed <sideeqkokori@gmail.com>
2023-03-26 04:22:13 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - start of code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(48 + i);
+	putchar('\n');
+	return (0);
+}
